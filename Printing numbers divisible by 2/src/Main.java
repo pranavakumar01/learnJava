41afdafa96785d3a1c2b1e98bef000344ef433db
pranava <pranavakumar01@gmail.com>
2024-07-07 15:17:06 +0530

    printing numbers divisible by 2

---
+++ Printing numbers divisible by 2/src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class main {
+    public static void main(String[] args) {
+        int n1 = 1;
+        int n2 = 100;
+        for (int i = n1; i < n2; i++) {
+            if (((i & 1) == 0)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
